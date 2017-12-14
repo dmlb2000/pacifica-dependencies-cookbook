f0723ff24ca47de4f19986186c2eaed1abfe1679
Brown <david.brown@pnnl.gov>
2017-12-14 14:21:07 -0800

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# pacifica-dependencies
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: pacifica-dependencies
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
