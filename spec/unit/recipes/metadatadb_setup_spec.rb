4101fc706646ced5522160d9cbd2f83319ef332a
David Brown <dmlb2000@gmail.com>
2017-12-15 19:49:44 -0800

    fix rspec...

--- spec/unit/recipes/metadatadb_setup_spec.rb
+++ spec/unit/recipes/metadatadb_setup_spec.rb
@@ -7,6 +7,11 @@
 require 'spec_helper'
 
 describe 'pacifica-dependencies::metadatadb_setup' do
+  before do
+    stub_command("psql -c '\\l' | grep -q pacifica_metadata").and_return(true)
+    stub_command("psql -c 'SELECT rolname FROM pg_roles;' | grep -q pacifica").and_return(true)
+    stub_command("psql -c '\\l' | grep -q pacifica=").and_return(true)
+  end
   context 'When all attributes are default, on an Ubuntu 16.04' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
