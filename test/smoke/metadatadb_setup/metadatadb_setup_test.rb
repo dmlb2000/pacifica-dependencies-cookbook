6389d0a726091856dcf4002496137c8ed3699913
David Brown <dmlb2000@gmail.com>
2017-12-15 20:48:12 -0800

    get the tests right for this one

--- test/smoke/metadatadb_setup/metadatadb_setup_test.rb
+++ test/smoke/metadatadb_setup/metadatadb_setup_test.rb
@@ -5,14 +5,14 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe command(%(su - postgres -c "psql -c '\\l' | grep -q pacifica_metadata")) do
+  its(:exit_status) { should eq 0 }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe command(%(su - postgres -c "psql -c 'SELECT rolname FROM pg_roles;' | grep -q pacifica")) do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command(%(su - postgres -c "psql -c '\\l' | grep -q pacifica=")) do
+  its(:exit_status) { should eq 0 }
 end
