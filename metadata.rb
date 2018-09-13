5289778cac34c4762339ba4e844aa285491925f2
David Brown <dmlb2000@gmail.com>
2018-09-13 09:23:35 -0700

    v0.2.0 Chef 14 Compatibility
    * Update to use chef 14
    * No longer testing CentOS 6
    * Minor Pipeline Fixes
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'dmlb2000@gmail.com'
 license 'Apache-2.0'
 description 'Installs/Configures pacifica-dependencies'
 long_description 'Installs/Configures pacifica-dependencies'
-version '0.1.2'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+version '0.2.0'
+chef_version '>= 14' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
@@ -20,11 +20,10 @@ issues_url 'https://github.com/pacifica/pacifica-dependencies/issues'
 source_url 'https://github.com/pacifica/pacifica-dependencies'
 
 supports 'ubuntu', '>= 16.04'
-supports 'centos', '>= 6.0'
-supports 'redhat', '>= 6.0'
+supports 'centos', '>= 7.0'
+supports 'redhat', '>= 7.0'
 
 depends 'apt'
-depends 'build-essential'
 depends 'chef-sugar'
 depends 'rabbitmq'
 depends 'java'
